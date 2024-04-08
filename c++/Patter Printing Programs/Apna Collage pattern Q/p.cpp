85b7079665ed3197aa8d56b70c7be37b61c76fa2
ElephantRifle <dhirendrakumar300200@gmail.com>
2024-04-08 23:21:02 +0530

    new

--- c++/Patter Printing Programs/Apna Collage pattern Q/p.cpp
+++ c++/Patter Printing Programs/Apna Collage pattern Q/p.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    //Rectangle 
     int col{},row{};
     cout<<"Enter col: ";
     cin>>col;
 
     cout<<"Enter row: ";
     cin>>row;
+    cout<<"\n";
 
+    cout<<"Rectangle"<<endl; 
     for(int i{1};i <=row;i++)
     {
         for(int j{1};j <= col;j++)
@@ -20,7 +21,7 @@ int main(){
 
     cout<<"=========================================="<<endl;
 
-    //Hollow Rectangle
+    cout<<"Hollow Rectangle"<<endl;
 
     for(int i{1};i <= row;i++){
         for(int j{1};j <=col;j++){
@@ -35,7 +36,7 @@ int main(){
 
     cout<<"=========================================="<<endl;
 
-    //Invered Half Pyramid
+    cout<<"Invered Half Pyramid"<<endl;
     int n{};
     cout<<"Enter a number: ";
     cin>>n;
@@ -59,7 +60,7 @@ int main(){
 
     cout<<"=========================================="<<endl;
 
-    //Opposite Triangle
+    cout<<"Opposite Triangle"<<endl;
     for(int i{1};i <= n;i++){
         for(int k{1}; k <= i;k++){
             cout<<" ";
@@ -73,7 +74,7 @@ int main(){
     cout<<"=========================================="<<endl;
 
 
-    //Half Pyramid after 180 degree rotation
+    cout<<"Half Pyramid after 180 degree rotation"<<endl;
     for(int i{1};i <= n;i++){
         for(int k{n-i};k >= 1;k--){
             cout<<"  ";
@@ -85,7 +86,7 @@ int main(){
     }
     
 
-    //Better Approach
+    cout<<"Better Approach"<<endl;
     for(int i{1};i <= n;i++){
         for(int j{1};j <= n;j++){
             if(j <= n-i){
@@ -99,7 +100,7 @@ int main(){
     }
     cout<<"=========================================="<<endl;
 
-    //Half Pyramid Using numbers
+    cout<<"Half Pyramid Using numbers"<<endl;
     for(int i{1};i <= n;i++){
         for(int j{n};j > n-i;j--){
             cout<<i<<" ";
@@ -108,7 +109,198 @@ int main(){
     }
 
     cout<<"=========================================="<<endl;
+
+    cout<<"Floyd's Triangle"<<endl;
+    int count_1{1};
+    for(int i{1};i <= n;i++){
+        for(int j{1};j <=i;j++){
+            cout<<count_1<<" ";
+            count_1++;
+        }
+        cout<<endl;
+    }
+
+    cout<<"=========================================="<<endl;
+    cout<<"Butter Fly Pattern"<<endl;
+    //Upper Half
+    for(int i{1};i <= n;i++){
+        for(int j{1};j <= i;j++){
+            cout<<"*";
+        }
+        for(int k{1};k <= 2*n - 2*i;k++){
+            cout<<" ";
+        }
+        for(int j{1};j <= i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    //Lower Half
+    // n-1 cuz we were getting extra line of * after the first half completes its execution   
+       for(int i{n-1};i >= 1;i--){
+        for(int j{1};j <= i;j++){
+            cout<<"*";
+        }
+        for(int k{1};k <= 2*n - 2*i;k++){
+            cout<<" ";
+        }
+        for(int j{1};j <= i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+       }
+
+    cout<<"=========================================="<<endl;
+    cout<<"Diamond Pattern"<<endl;
+
+
+    for(int i{1};i <= n;i++){
+        for(int k{n-i};k >= 1;k--){
+            cout<<" ";
+        }
+        for(int j{1};j <= i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i{1};i <= n;i++){
+        for(int k{1};k <= i;k++){
+            cout<<" ";
+        }
+        for(int j{n-i};j >= 1;j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"=========================================="<<endl;
+
+    cout<<"Inverted Pattern"<<endl;
+
+    int count_2{1};
+    for(int i{1};i <= n;i++){
+        for(int j{n+1-i};j >=1;j--){
+            cout<<count_2<<" ";
+            count_2++;
+        }
+        cout<<endl;
+        count_2 = 1;
+    }
+    //OR
+    for(int i{1};i <= n;i++){
+        for(int j{1};j <= n+1-i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"=========================================="<<endl;
+
+    cout<<"0 - 1 Pattern"<<endl;
+
+    for(int i{1};i <= n;i++){
+        for(int j{1};j <= i;j++){
+            if((i + j) % 2 == 0){
+                cout<<" 1";
+            }else{
+                cout<<" 0";
+            }
+        }
+        cout<<endl;
+    }
+    cout<<"=========================================="<<endl;
+
+    cout<<"Rhombus Pattern "<<endl;
+
+    for(int i{1};i <= n;i++){
+        for(int k{1}; k <= n-i;k++){
+            cout<<" ";
+        }
+        for(int j{1};j <= n;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"=========================================="<<endl;
+
+    cout<<"Number Pattern"<<endl;
+
+    for(int i{1};i <= n;i++){
+        for(int k{1};k <= n-i;k++){
+            cout<<" ";
+        }
+        for(int j{1};j <= i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"=========================================="<<endl;
+
+    cout<<"Palindromic Pattern"<<endl;
+
+    for(int i{1};i <= n;i++){
+        for(int k{1};k <= n-i;k++){//For Space
+            cout<<"  ";
+        }
+        for(int j{i};j >=1;j--){//For Printing row no to 1 (lest handside)
+            cout<<j<<" ";
+        }
+        for(int j{2};j <= i;j++)//For Printing from 1 to row no(right handside)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<"=========================================="<<endl;
+
+    cout<<"Star Pattern"<<endl;
+    //First Half
+    for(int i{1};i <= n;i++){
+        for(int k{1};k <=n-i;k++){
+            cout<<"  ";
+        }
+        for(int j{1};j <= (2*i)-1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    //Second Half
+    for(int i{n};i >= 1;i--){
+        for(int k{i};k < n;k++){
+            cout<<"  ";
+        }
+        for(int j{(2*i)-1};j>=1;j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<"=========================================="<<endl;
+
+    cout<<"Zig - Zag Pattern"<<endl;
+
+    for(int i{1};i <= 3;i++){
+        for(int j{1}; j <= n;j++){
+            if((i+j) % 4 == 0 || (i == 2 && j % 4 == 0) ){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+
+    cout<<"=========================================="<<endl;
+
+    cout<<" Hollow Butterfly Pattern"<<endl;
+
+    cout<<"=========================================="<<endl;
     
+
+
 
     return 0;
 }
---
+++ c++/Patter Printing Programs/Apna Collage pattern Q/question.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+// //Prime Number
+// int main(){
+//     int number{};
+//     cout<<"Enter a number: ";
+//     cin>>number;
+//     bool prime{true};
+//     for(int i{2};i <= sqrt(number);i++){
+//         if(number % i == 0){
+//             prime = false;
+//             break;
+//         }
+//     }
+//     if(prime){
+//         cout<<"Is Prime"<<endl;
+//     }else{
+//         cout<<"Not A Prime"<<endl;
+//     }
+//     return 0;
+// }
+//Reverse l_digit number
+//(1)
+// int main(){
+//     int number{};
+//     cout<<"Enter a number: ";
+//     cin>>number;
+
+//     string reverse = to_string(number);
+    
+//     int size = reverse.length();
+//     string reverse_1{};
+
+//     for(int i{size-1};i >= 0;i--){
+//         reverse_1+= reverse[i];
+//     }
+//     cout<<reverse_1;
+
+//     return 0;
+// }
+//(2)
+// int main(){
+//     int number{};
+//     int reverse{};
+//     cout<<"Enter a number: ";
+//     cin>>number;
+
+//     while(number>0){
+//         int last_digit = number%10;
+//         reverse = reverse*10 + last_digit;
+//         number= number/10;
+//     }
+//     cout<<reverse<<endl;
+
+//     return 0;
+// }
+
+
+//Check if a given number is a Armstrong number
+int main(){
+    int n{},sum{};
+
+    cout<<"Enter a number: ";
+    cin>>n;
+    
+    int original_no = n;
+    
+    while(n > 0){
+        int l_digit = n % 10;
+        sum = sum+(l_digit*l_digit*l_digit);
+        n = n/10;
+  }
+  if(sum == original_no){
+    cout<<"Number is a Armstrong number"<<endl;
+  }else{
+    cout<<"It is not a Armstrong number "<<endl;
+  }
+  
+
+    return 0;
+}
